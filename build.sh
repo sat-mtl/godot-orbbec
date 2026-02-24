bc66d9080845ca1334e37ca2f8e29377a5361633
Guillaume Riou <griou@sat.qc.ca>
2026-02-24 12:59:56 -0500

    copy the .so where the ci think it is when the build is done

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@
 
 cmake -B build -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_C_COMPILER="clang"
 make -sC build -j8
+
+cp build/lib/godot-orbbec.linux.template* ./bin/linux/
+echo "aaaaaaaaaa"
