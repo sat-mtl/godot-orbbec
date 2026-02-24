304360c53b2b607bacc8af702783a9a39dab63ed
guillaume riou <griou@sat.qc.ca>
2026-02-24 17:14:26 -0500

    maaaaaybe ?

--- build_windows.cmd
+++ build_windows.cmd
@@ -41,9 +41,9 @@ echo ============================================
 echo ============================================
 echo  Configuring project with CMake
 echo ============================================
-
+IF not DEFINED BUILD_RELEASE (ECHO BUILD_RELEASE IS defined) ELSE (ECHO BUILD_RELEASE is NOT defined)
 echo !BUILD_RELEASE!
-if !BUILD_RELEASE!=="" (
+if not defined BUILD_RELEASE (
     :: if BUILD_RELEASE is unset, build in debug mode
     cmake -B build -DCMAKE_POLICY_VERSION_MINIMUM="3.5"
 ) else (
@@ -56,7 +56,7 @@ if !BUILD_RELEASE!=="" (
 echo ============================================
 echo  Building project
 echo ============================================
-if !BUILD_RELEASE!=="" (
+if not defined BUILD_RELEASE (
    cmake --build build
 ) else (
    cmake --build build --config Release
